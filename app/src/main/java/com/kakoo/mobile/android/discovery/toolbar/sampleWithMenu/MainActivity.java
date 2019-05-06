250c003d96fe204cb7fdf06ec6277df5aca7a692
Resse Christophe <christophe.resse@gmail.com>
2019-05-06 12:32:41 +0200

    Initial commit

---
+++ app/src/main/java/com/kakoo/mobile/android/discovery/toolbar/sampleWithMenu/MainActivity.java
@@ -0,0 +1,69 @@
+package com.kakoo.mobile.android.discovery.toolbar.sampleWithMenu;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings option menu clicked!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_add_contact:
+                Toast.makeText(this, "Add Contact option menu clicked!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_about_us:
+                Toast.makeText(this, "About Us option menu clicked!", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
